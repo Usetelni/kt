004f3b5220821615465e692248f13ac56d50643b
Thiago <thiago.usetelni@visionnaire.com.br>
2021-12-13 14:41:02 -0300

    Project wireframe.

---
+++ src/main/kotlin/com/teste/kt/testekt/TesteKtApplication.kt
@@ -0,0 +1,11 @@
+package com.teste.kt.testekt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TesteKtApplication
+
+fun main(args: Array<String>) {
+	runApplication<TesteKtApplication>(*args)
+}
